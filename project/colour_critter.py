99f6cc9c851367a2b3f42661ad7bc4de9695196e
philippkueppers@icloud.com <philippkueppers@icloud.com>
2026-01-22 17:16:37 +0100

    clean up file a bit

--- project/colour_critter.py
+++ project/colour_critter.py
@@ -151,28 +151,8 @@ with model:
     ahead_color = nengo.Node(look_ahead)    
     
     ### Agent functionality - your code adds to this section ###################
-
-    # random exploration? same as wall avoidance? 
-    # 
-
-    # when agent encounters a square, it should be able to correctly recognise it.
-    # use current_color. Make basal ganglia actions like in exercise 4.2 
-    # Rules: Compute dot(current_color, RED) -> action
     
-    # to detect 2 coloured sequences in a row: have some form of memory
-    # After seeing one color, there can be white tiles in between before the next. So keep looping color you have seen in memory? 
-    # Like evercise 4.2: keep looping last seen color, switch is then second color. And then second color becomes first color and wait for new switch
-    # for each colour that it encounters following red, it should count how often that happens
-    # Where to store the counters? In memory that decays? so counter resets eventually? 
-    # (last_seen=RED x curr_color=RED) and (seen_white) -> redCounter +=1 
-    # (last_seen=RED x curr_color=Any different) -> Any different Counter +=1 
-    # else: dont change counter
-    # Problem: How to make sure that after red there is one white square before red again?
-    # seen_white: maybe one ensemble that is just for counting if a white square has been visited, so 1. And goes to 0 if visit color
-    
-    # TODO: convert 0 to 1 colour input in -1 and 1 range to have large 
-    
-    model.cc = spa.State(D,vocab=vocab2) # represent the last seen color 
+    model.cc = spa.State(D,vocab=vocab2) # currently seen color by the agend
     
     def signal_to_spa(xs):
         """ Return spa vector corresponding to input
@@ -207,8 +187,7 @@ with model:
     
     nengo.Connection(current_color, model.cc.input,function=signal_to_spa)
     
-    # spa State = represent a vector, optionally with memory to remember the vector. 
-    model.seen_red = spa.State(D,vocab=vocab) # represent the last seen color 
+    model.seen_red = spa.State(D,vocab=vocab) # if the agend has seen red as the last color
     
     model.cleanup = spa.AssociativeMemory(input_vocab=vocab, wta_output=True)
     nengo.Connection(model.cleanup.output, model.seen_red.input, synapse=0.01)
@@ -232,12 +211,6 @@ with model:
     model.bg = spa.BasalGanglia(actions)
     model.thalamus = spa.Thalamus(model.bg)
     
-    # seen_white neuron:
-    # init state as YES
-    # get current_color as input
-    # if input == color : set state to NO
-    # if input == white: set state to YES
-    # use signal delays to time everything
     model.seen_white = spa.State(D,vocab=vocab)
     
     model.cleanup_sw = spa.AssociativeMemory(input_vocab=vocab, wta_output=True)
@@ -257,10 +230,6 @@ with model:
     model.bg_sw = spa.BasalGanglia(actions_sw)
     model.thalamus_sw = spa.Thalamus(model.bg_sw)
     
-    # increase_counter? neuron:
-    # (last_seen=RED x curr_color=RED) and (seen_white) -> redCounter +=1 
-    # (last_seen=RED x curr_color=Any different) -> Any different Counter +=1 
-    # have one counter per color. One counter is flicker wheel
     model.increase_red_counter = spa.State(D,vocab=vocab)
     model.increase_green_counter = spa.State(D,vocab=vocab)
     model.increase_blue_counter = spa.State(D,vocab=vocab)
@@ -276,14 +245,8 @@ with model:
         '0.4 --> increase_red_counter=10*NO, increase_green_counter=10*NO, increase_blue_counter=10*NO, increase_magenta_counter=10*NO, increase_yellow_counter=10*NO'
         )
     
-    # what about: boolean takes some time to flip to true via 0.5* but then instantly goes back to false
-    
     model.bg_ic = spa.BasalGanglia(actions_ic)
     model.thalamus_ic = spa.Thalamus(model.bg_ic)
-    
-    # for counter: can use one stepper per counter
-    # how to use vector convolved with itself? 
-    # or use analog clock counter? 
     
     tau = 0.1
     
@@ -402,46 +365,8 @@ with model:
     nengo.Connection(counter_y, counter_y, synapse=tau)
     nengo.Connection(trigger_node_y, counter_y, synapse=tau)
     
-    
-    # current color != ahead color: use this to detect color switch? 
-    
-    # flicker has switch, but node can also act as switch
-    # what to make of this? 
-    
-    # How to build?
-    # keep streaming current_color
-    # keep last_seen in memory
-    # if new different second color or red
-    # spike 1 and 
-    
-    # counter
-    # 5 counters for 5 different colours. 
-    # lecturer: use SPA or another approach from lecture 6 - does he mean "exploiting multiple time scales"?
-    # need to get something to a high value to cause a change in the counter
-    # want the counter to go to a high enough value 
-    # look at how analog clocks do this
-    
-    # convolving vector with one element with itself cycles element through
-    # use this as counter? 
-    # does this decay in memory? 
-    # better than using spa? 
-    
-    # if using SPA: do we need first node, then ensemble and then spa element? 
-    # or do you put them directly into spa? 
-
-    # If using look_ahead() function:
-    # if current color is red, increase counter of look_ahead by 1
-    # And then no need for memory of last seen color? 
-    # Problem: How to make sure that after red there is one white square before red again?
-
-    # to compare two spa actions: use spa.Compare
-    # you should be able to just include  + cmp on the basal ganglia rule
-    # SPA system create a network cmp = spa.Compare(dimensions) and use two cortical rules cmp_A = state1 and cmp_B = state2.
-    
     #All input nodes should feed into one ensemble. Here is how to do this for
     #the radar, see if you can do it for the others
-    # all input nodes should feed into the same ensemble? 
-    # the weights of walldist are tuned such that? 
     # walldist represents the wall distance as vector. 
     walldist = nengo.Ensemble(n_neurons=500, dimensions=3, radius=4) # group of neurons that represent a vector
     nengo.Connection(proximity_sensors, walldist)
